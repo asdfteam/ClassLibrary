2ee6a09c94cbe199228a18cb3b12a32436b48dd8
christianfoleide <christian.foleide@gmail.com>
2021-02-21 15:09:04 +0100

    added username fields for customers

--- Customer.cs
+++ Customer.cs
@@ -12,6 +12,10 @@ namespace HotelLibrary
         public int CustomerId { get; set;  }
         [Column("customerName")]
         public string CustomerName { get; set; }
+        [Column("password")]
+        public string Password { get; set; }
+        [Column("username")]
+        public string Username { get; set; }
 
         public Customer()
         {
---
+++ LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelLibrary
+{
+    class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public LoginRequest() { }
+
+    }
+}
