4b9ec8d10c9ca0d5de59ff795d9ecd14ac179622
christianfoleide <christian.foleide@gmail.com>
2021-02-21 19:25:00 +0100

    mapper row for getting all reservations

---
+++ CustomerReservation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelLibrary
+{
+    [Table("CustomerReservations")]
+    public class CustomerReservation
+    {
+
+        [Column("customerId")]
+        public int CustomerId { get; set; }
+        [Key]
+        [Column("reservationId")]
+        public int ReservationId { get; set; }
+        [Column("roomNumber")]
+        public int roomNumber { get; set; }
+
+        public CustomerReservation() { }
+    }
+}
